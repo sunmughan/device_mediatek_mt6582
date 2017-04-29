0c0dbee000c323aba2cb8758223bb00d70dfc424
Sunmughan Swamy <sunmughaN@Gmail.com>
2017-04-29 15:47:37 +0530

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -5,13 +5,13 @@ PRODUCT_RELEASE_NAME := MT6582
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit from hardware-specific part of the product configuration
-$(call inherit-product, device/mediatek/mt6582/device.mk)
+$(call inherit-product, device/Micromax/a106/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := mt6582
-PRODUCT_NAME := cm_mt6582
-PRODUCT_BRAND := Mediatek
-PRODUCT_MODEL := MT6582
-PRODUCT_MANUFACTURER := MediaTek
+PRODUCT_DEVICE := A106
+PRODUCT_NAME := cm_a106
+PRODUCT_BRAND := Micromax
+PRODUCT_MODEL := a106
+PRODUCT_MANUFACTURER := Micromax
 
 PRODUCT_GMS_CLIENTID_BASE := android-mediatek
