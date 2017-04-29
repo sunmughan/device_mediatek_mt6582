29da0698aed122f753d701c6c9002e26d76dbe78
Sunmughan Swamy <sunmughaN@Gmail.com>
2017-04-29 15:58:12 +0530

    Update AndroidBoard.mk

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -5,12 +5,12 @@ include $(CLEAR_VARS)
 ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
 # include the non-open-source counterpart to this file
--include vendor/mediatek/mt6582/AndroidBoardVendor.mk
+-include vendor/micromax/a106/AndroidBoardVendor.mk
 
 # copy keylayout overrides
 $(shell mkdir -p $(TARGET_OUT_KEYLAYOUT); \
-    cp -pf device/mediatek/mt6582/keylayout/Generic.kl $(TARGET_OUT_KEYLAYOUT))
+    cp -pf device/micromax/a106/keylayout/Generic.kl $(TARGET_OUT_KEYLAYOUT))
 
 # copy permissions overrides
 $(shell mkdir -p $(TARGET_OUT_ETC)/permissions; \
-    cp -pf device/mediatek/mt6582/configs/android.hardware.camera.xml $(TARGET_OUT_ETC)/permissions)
+    cp -pf device/micromax/a106/configs/android.hardware.camera.xml $(TARGET_OUT_ETC)/permissions)
